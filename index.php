8abd038caba02a728e90eed3ea3c8210e2ac85ad
MalinovskyM <bbriddler@gmail.com>
2020-02-28 15:15:51 +0200

    correction elevator

--- index.php
+++ index.php
@@ -2,54 +2,53 @@
 
 	class Elevator
 	{
+        private $door;
+        private $currentFloor = 5;
+        const maxWeight = 480;
 
-		private $door;
-		private $currentFloor = 5;
-		const maxWeight = 480;
+        private function openDoor()
+        {
+            $this->door = 'open';
+        }
 
-		private function openDoor()
-		{
-			$this->door = 'open';
-		}
+        private function closeDoor()
+        {
+            $this->door = 'close';
+        }
 
-		private function closeDoor()
-		{
-			$this->door = 'close';
-		}
+        private function checkWeight($weight)
+        {
+            return $weight < 480 ? 'ok' : 'overweight';
+        }
 
-		private function checkWeight($weight)
-		{
-			return $weight < 480 ? 'ok' : 'overweight';
-		}
+        public function help()
+        {
+            $status = 'call dispatcher';
+            return $status;
+        }
 
-		public function help()
-		{
-			$status = 'call dispatcher';
-			return $status;
-		}
+        public function move($numFloors, $weight)
+        {
+            $weightRes = $this->checkWeight($weight);
+            if ($weightRes=='ok') {
+                $this->closeDoor();
+                $direction = $this->currentFloor <=> $numFloors;
+                if($direction == 0) {
+                    $result = 'you are already on this floor';
+                } elseif ($direction == -1) {
+                    $countFlore = $numFloors - $this->currentFloor;
+                    $result = 'Move the elevator up '.$countFlore.' floors.';
+                } elseif ($direction == 1) {
+                    $countFlore = $this->currentFloor - $numFloors;
+                    $result = 'Move the elevator down '.$countFlore.' floors.';
+                }
+                $this->openDoor();
+            } else {
+                $result = $weightRes;
+            }
 
-		public function move($numFloors, $weight)
-		{
-			$weightRes = $this->checkWeight($weight);
-			if ($weightRes=='ok') {
-				$this->closeDoor();
-				$direction = $this->currentFloor <=> $numFloors;
-				if($direction == 0) {
-					$result = 'you are already on this floor';
-				} elseif ($direction == -1) {
-					$countFlore = $numFloors - $this->currentFloor;
-					$result = 'Move the elevator up '.$countFlore.' floors.';
-				} elseif ($direction == 1) {
-					$countFlore = $this->currentFloor - $numFloors;
-					$result = 'Move the elevator down '.$countFlore.' floors.';
-				}
-				$this->openDoor();
-			} else {
-				$result = $weightRes;
-			}
-
-			return $result;
-		}
+            return $result;
+        }
 	}
 
 
