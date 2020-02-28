a0cc7f8776df293c44b86a1942a4edc304f6d6fe
MalinovskyM <bbriddler@gmail.com>
2020-02-28 15:36:16 +0200

    correction elevator5

--- index.php
+++ index.php
@@ -1,53 +1,53 @@
 <?php
   class Elevator
   {
-	private $doorStatus;
-	private $currentFloor = 5;
-	const maxWeight = 480;
+    private $doorStatus;
+    private $currentFloor = 5;
+    const maxWeight = 480;
 
-	private function openDoor()
-	{
-	  $this->doorStatus = 'open';
+    private function openDoor()
+    {
+      $this->doorStatus = 'open';
+    }
+
+    private function closeDoor()
+    {
+      $this->doorStatus = 'close';
+    }
+
+    private function checkWeight($weight)
+    {
+      return $weight < self::maxWeight ? 'ok' : 'overweight';
+    }
+
+    public function help()
+    {
+      return 'call dispatcher';
 	}
 
-	private function closeDoor()
-	{
-	  $this->doorStatus = 'close';
-	}
+    public function move($floor, $weight)
+    {
+      $weightRes = $this->checkWeight($weight);
+      if ($weightRes == 'ok') {
+        $this->closeDoor();
+        $direction = $this->currentFloor <=> $floor;
+        if($direction == 0) {
+          $result = 'You are already on this floor';
+        } elseif ($direction == -1) {
+          $countFlore = $floor - $this->currentFloor;
+          $result = 'Move the elevator up '.$countFlore.' floors.';
+        } elseif ($direction == 1) {
+          $countFlore = $this->currentFloor - $floor;
+          $result = 'Move the elevator down '.$countFlore.' floors.';
+        }
+        $this->currentFloor = $floor;
+        $this->openDoor();
+      } else {
+        $result = $weightRes;
+      }
 
-	private function checkWeight($weight)
-	{
-	  return $weight < self::maxWeight ? 'ok' : 'overweight';
-	}
-
-	public function help()
-	{
-	  return 'call dispatcher';
-	}
-
-	public function move($floor, $weight)
-	{
-	  $weightRes = $this->checkWeight($weight);
-	  if ($weightRes == 'ok') {
-		$this->closeDoor();
-		$direction = $this->currentFloor <=> $floor;
-		if($direction == 0) {
-		  $result = 'You are already on this floor';
-		} elseif ($direction == -1) {
-		  $countFlore = $floor - $this->currentFloor;
-		  $result = 'Move the elevator up '.$countFlore.' floors.';
-		} elseif ($direction == 1) {
-		  $countFlore = $this->currentFloor - $floor;
-		  $result = 'Move the elevator down '.$countFlore.' floors.';
-		}
-		$this->currentFloor = $floor;
-		$this->openDoor();
-	  } else {
-		$result = $weightRes;
-	  }
-
-	  return $result;
-	}
+      return $result;
+    }
   }
 
 
